bcb5ee52006c3d938c96ad1c3cff5a9f1bffc696
Fernanda9421 <fer.candrade21@gmail.com>
2021-08-25 13:32:36 -0300

    Cria exemplos de variaveis com JS

---
+++ fundamentos/Bloco04-Introducao_JS/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Fernanda";
+const birthCity = "Campo Belo";
+let birthYear = 1994;
+
+birthYear = 2030;
+birthCity = "Belo Horizonte"; // Erro - Não se pode reatribuir valores para constantes
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
