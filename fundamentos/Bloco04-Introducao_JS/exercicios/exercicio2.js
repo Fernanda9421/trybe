02e6dfa987dfa2abef4479da2847e6da80cbe1a2
Fernanda9421 <fer.candrade21@gmail.com>
2021-08-26 09:48:13 -0300

    Resoluçao dos exercicios propostos (2 ao 6)

---
+++ fundamentos/Bloco04-Introducao_JS/exercicios/exercicio2.js
@@ -0,0 +1,10 @@
+const num1 = 40;
+const num2 = 25;
+
+if (num1 > num2) {
+    console.log (num1);
+} else if (num1 < num2) {
+    console.log(num2)
+} else {
+    console.log ('números são iguais')
+}
