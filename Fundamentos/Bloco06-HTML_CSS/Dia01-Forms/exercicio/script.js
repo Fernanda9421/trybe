2d8a7b7c5296e90c61061e75a2ecd36c2eaaf37e
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-16 18:46:58 -0300

    Ajusta arquivos

--- Fundamentos/Bloco06-HTML_CSS/Dia01-Forms/exercicio/script.js
+++ Fundamentos/Bloco06-HTML_CSS/Dia01-Forms/exercicio/script.js
@@ -29,7 +29,7 @@ const states = {
 };
 
 const statesOfBrazil = () => {
-  const getSelect = document.querySelector('#state');
+  const getSelect = document.querySelector('#state');  
   for (const key in states) {
     let options = document.createElement('option');
     options.innerText = states[key]
@@ -39,75 +39,3 @@ const statesOfBrazil = () => {
   }
 }
 statesOfBrazil();
-
-const inputs = {
-  name: {
-    maxLength: 40,
-    required: true
-  },
-  email: {
-    maxLength: 50,
-    required: true
-  },
-  cpf: {
-    maxLength: 11,
-    required: true
-  },
-  adress: {
-    maxLength: 200,
-    required: true
-  },
-  city: {
-    maxLength: 28,
-    required: true
-  },
-  state: {
-    type: 'select',
-    required: true
-  },
-  type: {
-    type: 'radio',
-    required: true
-  },
-  abstract: {
-    maxLength: 1000,
-    required: true
-  },
-  position: {
-    maxLength: 40,
-    required: true
-  },
-  positionDescription: {
-    maxLength: 500,
-    required: true
-  },
-  date: {
-    type: 'date',
-    required: true
-  }
-};
-
-const getButton = document.querySelector('#button');
-const checkDate = () => {
-  const inputDate = document.querySelector('#date').value;
-  const separateDate = inputDate.split('/');
-  const numberOne = parseInt(separateDate[0]);
-  const numberTwo = parseInt(separateDate[1]);
-  const numberThree = parseInt(separateDate[2]);
-
-  if (numberOne < 0 || numberOne > 31) {
-    alert('Dia de início inválido')
-  }
-  if (numberTwo < 0 || numberTwo > 12) {
-    alert('Mês de início inválido')
-  }
-  if (numberThree <= 0) {
-    alert('Ano de início inválido')
-  }
-}
-getButton.addEventListener('click', checkDate)
-
-function preventDefault (event) {
-  event.preventDefault();
-}
-getButton.addEventListener('click', preventDefault);
