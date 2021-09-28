4dc0212495500b9b286710f5aa86f5265865ad0d
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-28 11:15:57 -0300

    Adiciona resoluçao de exercicios bonus

--- Fundamentos/Bloco07-JS-ES6_Testes-unitarios/Dia02-Objects/exercicios/script2.js
+++ Fundamentos/Bloco07-JS-ES6_Testes-unitarios/Dia02-Objects/exercicios/script2.js
@@ -32,7 +32,7 @@ const listKeys = (object) => {
   const listKeys = Object.keys(object);
   return listKeys;
 }
-console.log(listKeys(lesson1));
+//console.log(listKeys(lesson1));
 
 // Tamanho de um obejto:
 // const objectLength = (object) => {
@@ -40,11 +40,11 @@ console.log(listKeys(lesson1));
 //   return length;
 // }
 const objectLength = (object) => Object.keys(object).length;
-console.log(objectLength(lesson1));
+//console.log(objectLength(lesson1));
 
 // Listar valores de um objeto:
 const listValues = (object) => Object.values(object);
-console.log(listValues(lesson1));
+//console.log(listValues(lesson1));
 
 // Agrupar todos os objetos em um:
 
@@ -65,17 +65,48 @@ const sumStudents = (object) => {
   }
   return sumStudents
 }
-console.log(sumStudents(allLessons));
+//console.log(sumStudents(allLessons));
 
 // Obter o valor da chave de acordo com sua posição no objeto:
 const getValueByNumber = (object, index) => {
   const values = Object.values(object)[index];
   return values
 }
-console.log(getValueByNumber(lesson1, 0));
+//console.log(getValueByNumber(lesson1, 0));
 
 // Verificar se o par (chave/valor) existe na função:
 const verifyPair = (object, key, value) => {
   return object[key] === value;
 }
-console.log(verifyPair(lesson1, 'professor', 'Maria Clara'));
+//console.log(verifyPair(lesson1, 'professor', 'Maria Clara'));
+
+// BÔNUS: Contar quantos estudantes assistiram às aulas de Matemática:
+const mathClass = (object) => {
+  let students = 0;
+  const values = Object.values(object);
+  for (index in values) {
+    if (values[index].materia === 'Matemática') {
+      students += values[index].numeroEstudantes;
+    }
+  }
+  return `${students} estudantes assistiram a áula de Matemática`;
+}
+//console.log(mathClass(allLessons));
+
+// BÔNUS 2: Retornar um objeto com o relatório do professor:
+const createReport = (object, value) => {
+  let aulas = [];
+  let numeroEstudantes = 0;
+  let professor;
+  const values = Object.values(object);
+  for (index in values) {
+    if (values[index].professor === value) {
+      professor = values[index].professor;
+      aulas.push(values[index].materia);
+      numeroEstudantes += values[index].numeroEstudantes;
+    }
+  }
+  let newObject = Object.assign({}, {professor}, {aulas}, {numeroEstudantes});
+  return newObject;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
