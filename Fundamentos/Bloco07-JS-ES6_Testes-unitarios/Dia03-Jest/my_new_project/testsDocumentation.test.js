1912ca5637c595efcd23296b00e9efffdf0f7505
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-29 07:50:22 -0300

    Matcher para exceçao - toThrow

--- Fundamentos/Bloco07-JS-ES6_Testes-unitarios/Dia03-Jest/my_new_project/testsDocumentation.test.js
+++ Fundamentos/Bloco07-JS-ES6_Testes-unitarios/Dia03-Jest/my_new_project/testsDocumentation.test.js
@@ -79,3 +79,22 @@ test('this house has my desired features', () => {
   expect(houseForSale).toHaveProperty(['kitchen', 'nice.oven']);
   expect(houseForSale).not.toHaveProperty(['kitchen', 'open']);
 });
+
+// MATCHER PARA EXCEÇÕES:
+const multiplyByTwo = (number) => {
+  if (!number) {
+    throw new Error('number é indefinido')
+  }
+  return number * 2;
+};
+//multiplyByTwo(4);
+
+test('testa se multiplyByTwo retorna o resultado da multiplicação', () => {
+  expect(multiplyByTwo(4)).toBe(8);
+});
+test('testa se é lançado um erro quando number é indefinido', () => {
+  expect(() => { multiplyByTwo() }).toThrow();
+});
+test('testa se a mensagem de erro é "number é indefinido"', () => {
+  expect(() => { multiplyByTwo() }).toThrowError(new Error('number é indefinido'));
+});
