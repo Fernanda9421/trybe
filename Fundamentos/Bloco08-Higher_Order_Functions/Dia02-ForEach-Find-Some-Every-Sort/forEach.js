77bded9cfcfd9821411232ea5e71e631ea848862
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-05 18:08:14 -0300

    forEach

---
+++ Fundamentos/Bloco08-Higher_Order_Functions/Dia02-ForEach-Find-Some-Every-Sort/forEach.js
@@ -0,0 +1,18 @@
+// forEach para realizar a tabuada do 2:
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+// EXEMPLO 2:
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
