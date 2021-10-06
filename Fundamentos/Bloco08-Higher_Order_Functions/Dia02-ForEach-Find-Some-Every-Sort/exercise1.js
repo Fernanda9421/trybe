c5b9e990d4ffdd30b7ce4ab87291f5b1211c9f71
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-06 12:19:39 -0300

    Adiciona exercicios - 2 ao 5

--- Fundamentos/Bloco08-Higher_Order_Functions/Dia02-ForEach-Find-Some-Every-Sort/exercise1.js
+++ Fundamentos/Bloco08-Higher_Order_Functions/Dia02-ForEach-Find-Some-Every-Sort/exercise1.js
@@ -70,3 +70,95 @@ function authorBornIn1947() {
 }
 
 assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+// 2. Retorne o nome do livro de menor nome.
+function smallerName() {
+  let nameBook;
+  let arrayBooks = [];
+
+  books.forEach((book) => arrayBooks.push(book.name));
+  arrayBooks.sort((a, b) => a.length - b.length);
+
+  return arrayBooks[0];
+}
+
+assert.strictEqual(smallerName(), 'Duna');
+
+// 3. Encontre o primeiro livro cujo nome possui 26 caracteres.
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return bookWith26characters = books.find((nameBook) => nameBook.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
+
+// 4.Ordene os livros por data de lançamento em ordem decrescente.
+const expectedResult2 = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  return books.sort((a, b) => b.releaseYear - a.releaseYear);
+}
+
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult2);
+
+// 5. Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+const expectedResult3 = false;
+
+function everyoneWasBornOnSecXX() {
+  return books.every((bornOnSecXX) => bornOnSecXX.author.birthYear >= 1901 && bornOnSecXX.author.birthYear <= 2000);
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult3);
