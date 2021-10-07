8fb4668f17748a36d4604cf00426b2423e04f31d
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-07 10:02:35 -0300

    Exercicio 4

--- Fundamentos/Bloco08-Higher_Order_Functions/Dia03-Filter-Map/exercises/exercise1.js
+++ Fundamentos/Bloco08-Higher_Order_Functions/Dia03-Filter-Map/exercises/exercise1.js
@@ -154,3 +154,35 @@ function fantasyOrScienceFiction() {
   return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
 }
 assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult3);
+
+// 4. Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+const expectedResult4 = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  return books.filter((book) => {
+    return oldBooks = 2021 - book.releaseYear > 60;
+  }).sort((a, b) => a.releaseYear - b.releaseYear);
+}
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult4);
