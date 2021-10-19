acf4f088ed535d1ee3160c44465cb3ffc01ae584
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-19 10:50:51 -0300

    Exercicio 2 - converte crypto moedas

--- Fundamentos/Bloco09-Js-Testes_Assincronos/Dia02-Fetch_API-Async_await/exercises/crypto.js
+++ Fundamentos/Bloco09-Js-Testes_Assincronos/Dia02-Fetch_API-Async_await/exercises/crypto.js
@@ -1,30 +1,3 @@
-// const fetchCrypto = async () => {
-//   const url = 'https://api.coincap.io/v2/assets';
-
-//   const coins = await fetch(url)
-//   .then((response) => response.json())
-//   .then((data) => data.data)
-//   .catch((error) => error.toString());
-
-//   return coins;
-// }
-
-// const append = async () => {
-//   const cryptos = await fetchCrypto();
-
-//   const listCoins = document.querySelector('#list-coins');
-//   cryptos
-//   .filter((crypto, index) => index < 10)
-//   .forEach((crypto) => {
-//     const li = document.createElement('li');
-//     li.innerText = `${crypto.id}(${crypto.symbol}): ${Number(crypto.priceUsd).toFixed(2)}`;
-
-//     listCoins.appendChild(li);
-//   })
-// }
-
-// window.onload = () => append();
-
 const fetchCrypto = async () => {
   const url = 'https://api.coincap.io/v2/assets';
 
@@ -36,14 +9,29 @@ const fetchCrypto = async () => {
   return coins;
 }
 
+
+const convertPrice = async () => {
+  const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
+  const convert = await fetch(url)
+  .then((response) => response.json())
+  .then((data) => data.usd)
+  .catch((error) => error.toString())
+
+  return convert;
+}
+
 const append = async () => {
   const cryptos = await fetchCrypto();
+  const priceConverted = await convertPrice();
+
   const ulCoins = document.querySelector('#list-coins');
   cryptos
   .filter((crypto, index) => index < 10)
   .forEach((crypto) => {
     const li = document.createElement('li');
-    li.innerText = `${crypto.id} (${crypto.symbol}): ${Number(crypto.priceUsd).toFixed(2)}`;
+    const usdPrice = Number(crypto.priceUsd);
+    const brlPrice = usdPrice * priceConverted.brl;
+    li.innerText = `${crypto.id} (${crypto.symbol}): ${brlPrice.toFixed(2)}`;
 
     ulCoins.appendChild(li);
   })
