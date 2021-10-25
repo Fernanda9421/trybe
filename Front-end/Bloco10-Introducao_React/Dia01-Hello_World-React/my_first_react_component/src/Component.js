df712d7241fc10b1fc986e4f42d7d38d02aba3eb
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-25 18:38:50 -0300

    Primeiro componente React de classe

---
+++ Front-end/Bloco10-Introducao_React/Dia01-Hello_World-React/my_first_react_component/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MyDescription extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Fernanda Andrade</h1>
+        <p>Sou estudante de Desenvolvimento Web na Trybe, uma escola que ensina a programar, a aprender e a trabalhar. Também graduada em Nutrição, e atualmente estou em transição de carreira para a área de tecnologia.</p>
+      </div>
+    );
+  }
+}
+
+export default MyDescription;
