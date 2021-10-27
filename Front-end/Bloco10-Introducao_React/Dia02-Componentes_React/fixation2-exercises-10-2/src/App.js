83d5e308116cb7a6aea411351a431dc057729af6
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-27 14:59:03 -0300

    Refatora App.js - Albuns dispostos em array de objetos

--- Front-end/Bloco10-Introducao_React/Dia02-Componentes_React/fixation2-exercises-10-2/src/App.js
+++ Front-end/Bloco10-Introducao_React/Dia02-Componentes_React/fixation2-exercises-10-2/src/App.js
@@ -4,39 +4,42 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
+    const albums = [
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+        title: 'Mylo Xyloto',
+        releaseDate: {
+          year: '2011',
+          month: '10',
+          day: '24',
+        },
+        others: {
+          recordCompany: 'Capitol, Parlophone',
+          formats: 'CD, digital'
+        }
       },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
-      }
-    };
 
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
-      },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+        title: 'Ghost Stories',
+        releaseDate: {
+          year: '2014',
+          month: '05',
+          day: '16',
+        },
+        others: {
+          recordCompany: 'Parlophone',
+          formats: 'CD, digital'
+        }
       }
-    }
+    ]
 
     return (
-      <div className='div-albums'>
-        <Album album={ album01 } />
-        <Album album={ album02 } />
-      </div>
+      albums.map((album) => (
+        <div className='div-albums'>
+          <Album album={album} />
+        </div>
+      ))
     );
   }
 }
