caa41750887d65a9792277d03d2dc6edb92d8359
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-29 13:51:35 -0300

    Renderiza informaçoes na tela

--- Front-end/Bloco10-Introducao_React/Dia02-Componentes_React/app-tvshow/src/App.js
+++ Front-end/Bloco10-Introducao_React/Dia02-Componentes_React/app-tvshow/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React, { Component } from 'react';
+import catalog from './data';
+import TvShowList from './Components/TvShowList';
+import Header from './Components/Header';
 
-function App() {
-  return (
-    'Olá'
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <TvShowList catalog={catalog} />
+      </>
+    )
+  }
 }
 
 export default App;
