e6fd5ee5b7b92021ab1bb125b8df28b1486c6f4b
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-03 10:16:38 -0300

    Cria botoes e eventos - sintaxe incorreta

--- Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
+++ Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import React, { Component } from 'react';
 
+const messageOne = () => {
+  console.log('Botão 1: clicado')
+}
+
+const messageTwo = () => {
+  console.log('Botão 2: clicado')
+}
+
+const messageThree = () => {
+  console.log('Botão 3: clicado')
+}
+
 class App extends Component {
   render() {
-    return <div>Olá mundo!</div>
+    return (
+      <>
+      <button onClick={messageOne}>Botão 1</button>
+      <button onClick={messageTwo}>Botão 2</button>
+      <button onClick={messageThree}>Botão 3</button>
+      </>
+    );
   }
 }
 
