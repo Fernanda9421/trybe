562770f1f8122c23e457f76d491ab4748ea5b46f
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-03 16:00:03 -0300

    Troca cor de fundo do botao quando o numero for par, e mostra a cor no console

--- Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
+++ Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
@@ -19,28 +19,58 @@ class App extends Component {
   messageOne() {
     this.setState((previousState, _props) => ({
       clickNumber1: previousState.clickNumber1 + 1
-    }))
+    }), () => {
+      console.log(`Cor Botão 1: ${this.valueBtn(this.state.clickNumber1)}`)
+    });
   }
 
   messageTwo() {
     this.setState((previousState, _props) => ({
       clickNumber2: previousState.clickNumber2 + 1
-    }))
+    }), () => {
+      console.log(`Cor Botão 2: ${this.valueBtn(this.state.clickNumber2)}`)
+    })
   }
 
   messageThree() {
     this.setState((previousState, _props) => ({
       clickNumber3: previousState.clickNumber3 + 1
-    }))
+    }), () => {
+      console.log(`Cor Botão 3: ${this.valueBtn(this.state.clickNumber3)}`)
+    })
+  }
+
+  valueBtn(number) {
+    return number % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
+    const { clickNumber1, clickNumber2, clickNumber3 } = this.state;
+
     return (
-      <>
-      <button onClick={this.messageOne}>{this.state.clickNumber1}</button>
-      <button onClick={this.messageTwo}>{this.state.clickNumber2}</button>
-      <button onClick={this.messageThree}>{this.state.clickNumber3}</button>
-      </>
+      <section className='section-buttons'>
+        <h1>Handle Click</h1>
+        <button className='button'
+          onClick={this.messageOne}
+          style={{ backgroundColor: this.valueBtn(clickNumber1) }}
+        >
+          {clickNumber1}
+        </button>
+
+        <button className='button'
+          onClick={this.messageTwo}
+          style={{ backgroundColor: this.valueBtn(clickNumber2) }}
+        >
+          {clickNumber2}
+        </button>
+
+        <button className='button'
+          onClick={this.messageThree}
+          style={{ backgroundColor: this.valueBtn(clickNumber3) }}
+        >
+          {clickNumber3}
+        </button>
+      </section>
     );
   }
 }
