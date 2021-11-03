b35c32abbad8d9ac3f851c4a4947a63e05799431
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-03 12:57:27 -0300

    Declara as funçoes dentro da classe - constructor, super, this, bind

--- Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
+++ Front-end/Bloco11-Estado_Eventos_Formularios/Dia01-Componentes_com_Estado-Eventos/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React, { Component } from 'react';
 
-const messageOne = () => {
-  console.log('Botão 1: clicado')
-}
-
-const messageTwo = () => {
-  console.log('Botão 2: clicado')
-}
-
-const messageThree = () => {
-  console.log('Botão 3: clicado')
-}
-
 class App extends Component {
+  constructor() {
+    super()
+
+    this.messageOne = this.messageOne.bind(this);
+    this.messageTwo = this.messageTwo.bind(this);
+    this.messageThree = this.messageThree.bind(this);
+  }
+
+  messageOne() {
+    console.log('Botão 1: clicado')
+  }
+
+  messageTwo() {
+    console.log('Botão 2: clicado')
+  }
+
+  messageThree() {
+    console.log('Botão 3: clicado')
+  }
+
   render() {
     return (
       <>
-      <button onClick={messageOne}>Botão 1</button>
-      <button onClick={messageTwo}>Botão 2</button>
-      <button onClick={messageThree}>Botão 3</button>
+      <button onClick={this.messageOne}>Botão 1</button>
+      <button onClick={this.messageTwo}>Botão 2</button>
+      <button onClick={this.messageThree}>Botão 3</button>
       </>
     );
   }
