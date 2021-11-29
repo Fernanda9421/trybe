57a8569b6ec5a1ee09c68aea97c26f2d27c26d65
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-29 14:11:23 -0300

    Execicio 2

--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/exercise01/service.test.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/exercise01/service.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const service = require('./service');
 
 test('Testa a função randomNumber', () => {
@@ -6,4 +7,13 @@ test('Testa a função randomNumber', () => {
   expect(service.randomNumber()).toBe(10);
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
-})
+});
+
+test('Testa nova funcionalidade da função', () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((num1, num2) => num1 / num2);
+
+  expect(service.randomNumber(100, 5)).toBe(20);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledWith(100, 5);
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
