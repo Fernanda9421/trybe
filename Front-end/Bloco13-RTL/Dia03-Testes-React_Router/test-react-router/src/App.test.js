c9428e544fae13705c53e25eefce23e718eb0f61
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-29 15:38:29 -0300

    Teste 4 - testando apenas o componente About

--- Front-end/Bloco13-RTL/Dia03-Testes-React_Router/test-react-router/src/App.test.js
+++ Front-end/Bloco13-RTL/Dia03-Testes-React_Router/test-react-router/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import App from './App';
+import App, { About } from './App';
 
 describe('teste da aplicação toda', () => {
   it('deve renderizar o componente App', () => {
@@ -37,5 +37,14 @@ describe('teste da aplicação toda', () => {
     const notFoundTitle = screen.getByRole('heading',
       { name: 'Página não encontrada' });
     expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  // Testando o componente separadamente:
+  it('deve renderizar o componente About (apenas componente)', () => {
+    renderWithRouter(<About />);
+  
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
   });
 });
