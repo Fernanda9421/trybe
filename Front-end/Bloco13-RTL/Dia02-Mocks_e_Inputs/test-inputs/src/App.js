b3143690ff6d23679b505669af7757c8ee78e28d
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-29 10:57:15 -0300

    Testa campo de nome

--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
 
   handleInput(e) {
     const { name, value } = e.target;
-    console.log(name, value);
     this.setState({ [name]: value });
   }
 
--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.test.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('altera valor dos campos e testa o valor guardado', () => {
+  render (<App />);
+  const inputNome = screen.getByRole('textbox', { name: /nome/i });
+  expect(inputNome).toBeInTheDocument();
+  expect(inputNome).toHaveValue('');
+
+  const valueInputNome = 'Estudante da Trybe';
+  userEvent.type(inputNome, valueInputNome);
+  expect(inputNome).toHaveValue(valueInputNome);
 });
