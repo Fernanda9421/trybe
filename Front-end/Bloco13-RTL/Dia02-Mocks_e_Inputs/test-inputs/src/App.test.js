1ade9b0ef3b8c311958302b1d5af44b8d6bb254a
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-29 11:02:58 -0300

    Testa campo de email

--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.test.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/test-inputs/src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('altera valor dos campos e testa o valor guardado', () => {
+test('altera valor do campo de nome e verifica valor guardado', () => {
   render (<App />);
   const inputNome = screen.getByRole('textbox', { name: /nome/i });
   expect(inputNome).toBeInTheDocument();
@@ -12,4 +12,15 @@ test('altera valor dos campos e testa o valor guardado', () => {
   const valueInputNome = 'Estudante da Trybe';
   userEvent.type(inputNome, valueInputNome);
   expect(inputNome).toHaveValue(valueInputNome);
+});
+
+test('Testa campo de email e verifica valor guardado', () => {
+  render(<App />);
+  const inputEmail = screen.getByRole('textbox', { name: /email/i });
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveValue('');
+
+  const valueInputEmail = 'email@trybe.com';
+  userEvent.type(inputEmail, valueInputEmail);
+  expect(inputEmail).toHaveValue(valueInputEmail);
 });
