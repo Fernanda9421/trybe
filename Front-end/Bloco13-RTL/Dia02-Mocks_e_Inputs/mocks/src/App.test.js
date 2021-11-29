904fce4bd33a434158bcd16912149095d376cec4
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-29 10:25:56 -0300

    Implementa testes

--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/mocks/src/App.test.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/mocks/src/App.test.js
@@ -1,8 +1,31 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+//  Faz com que, após cada teste, o mock seja limpo.
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  // A constante joke cria um objeto similar ao que é retornado da API
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  // O jest.spyon espiona as chamadas a função fetch do objeto global
+  // Quando a função fetch for chamada, ao invés de fazer uma requisição a uma API externa será chamando nosso mock .
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
 });
