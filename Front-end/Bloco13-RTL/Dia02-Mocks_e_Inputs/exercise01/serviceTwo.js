8078d5733356700d9b7e399e90aace8bb90f6bb9
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-30 08:34:13 -0300

    Cria funçoes para o exercicio 4

---
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/exercise01/serviceTwo.js
@@ -0,0 +1,17 @@
+const upperCase = (string) => {
+  return string.toUpperCase();
+}
+
+const firstLetter = (stringTwo) => {
+  return stringTwo[0]
+}
+
+const concat = (a, b) => {
+  return `${a}${b}`;
+}
+
+module.export = {
+  upperCase,
+  firstLetter,
+  concat,
+};
