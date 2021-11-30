0128bdb37e7480f8f7d4ff560640e0ea43111432
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-30 07:35:13 -0300

    Exercicio 3 - mockReset

--- Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/exercise01/service.test.js
+++ Front-end/Bloco13-RTL/Dia02-Mocks_e_Inputs/exercise01/service.test.js
@@ -17,3 +17,24 @@ test('Testa nova funcionalidade da função', () => {
   expect(service.randomNumber).toHaveBeenCalledWith(100, 5);
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
+
+describe('Testa implementações', () => {
+  it('Retorna multiplicação dos parâmetros', () => {
+    service.randomNumber = jest.fn().mockImplementation((num1, num2, num3) => num1 * num2 * num3);
+    expect(service.randomNumber(100, 5, 2)).toBe(1000);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toBeCalledWith(100, 5, 2);
+    expect(service.randomNumber).toBeCalledTimes(1);
+  });
+  
+  it('Reseta implementação e cria nova que retorna o dobro do valor' , () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toBeCalledTimes(0);
+    service.randomNumber = jest.fn().mockImplementation((number) => number * 2);
+    expect(service.randomNumber(10)).toBe(20);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledWith(10);
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  })
+})
+
