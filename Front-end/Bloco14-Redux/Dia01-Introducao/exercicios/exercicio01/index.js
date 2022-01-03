25c91c0a4dc2f050818337fea657ec12538ab14a
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-03 14:59:39 -0300

    Cria reducer retornando estado inicial como default do switch

--- Front-end/Bloco14-Redux/Dia01-Introducao/exercicios/exercicio01/index.js
+++ Front-end/Bloco14-Redux/Dia01-Introducao/exercicios/exercicio01/index.js
@@ -1,8 +1,15 @@
 const Redux = require('redux');
 
-const store = Redux.createStore();
-
 const ESTADO_INICIAL = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer);
