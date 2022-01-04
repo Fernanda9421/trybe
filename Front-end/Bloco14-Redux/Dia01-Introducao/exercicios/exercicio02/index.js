54a89f35f3c4e70285cdb620306357f7ed5e42d1
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-04 09:32:40 -0300

    Cria Action Types, Actions e Reducer

--- Front-end/Bloco14-Redux/Dia01-Introducao/exercicios/exercicio02/index.js
+++ Front-end/Bloco14-Redux/Dia01-Introducao/exercicios/exercicio02/index.js
@@ -1,2 +1,40 @@
-// CRIAÇÃO DO STORE
-const store = Redux.createStore();
+// ACTION TYPES
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// ACTIONS
+const actionInc = () => {
+  store.dispatch({ type: INCREMENT });
+};
+
+const actionDec = () => {
+  store.dispatch({ type: DECREMENT });
+};
+
+const INITIAL_STATE = {
+  number: 0,
+};
+
+// REDUCER
+const reducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        number: state.number + 1,
+      }
+    case DECREMENT:
+      return {
+        number: state.number - 1,
+      }
+    default:
+      return state;
+  }
+}
+
+// STORE
+const store = Redux.createStore(reducer);
+
+store.subscribe(() => {
+  const { number } = store.getState();
+  console.log(number);
+})
