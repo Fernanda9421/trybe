017965f04bb34e7c7efdffbe2b6560ca5b3f85dc
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-05 10:10:47 -0300

    Renderiza componente InputList em App

--- Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/App.js
+++ Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import InputList from './Components/InputList';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        To Do List
-      </div>
+      <InputList />
     );
   }
 }
