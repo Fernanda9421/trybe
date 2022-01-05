081b7ff25eeb58b8b552b2a734933048eeaded19
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-05 10:57:00 -0300

    Configura mapStateToProps e renderiza itens da lista

--- Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/Components/List.js
+++ Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/Components/List.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class List extends Component {
   render() {
+    const { list } = this.props;
     return (
       <div>
-        
+        <div>
+          {
+            list.map((item, index) => (
+              <p key={ index }>{ item }</p>
+            ))
+          }
+        </div>
       </div>
     )
   }
 }
 
-export default List;
+const mapStateToProps = (state) => ({
+  list: state.myReducer,
+});
+
+export default connect(mapStateToProps)(List);
