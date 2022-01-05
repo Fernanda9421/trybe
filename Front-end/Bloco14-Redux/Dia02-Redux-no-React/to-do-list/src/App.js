9a3d66b341b94cb0f3a8c409b3052e0a1c9ed94b
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-05 10:57:57 -0300

    Renderiza componente List

--- Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/App.js
+++ Front-end/Bloco14-Redux/Dia02-Redux-no-React/to-do-list/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import InputList from './Components/InputList';
+import List from './Components/List';
 
 class App extends Component {
   render() {
     return (
-      <InputList />
+      <>
+        <InputList />
+        <List />
+      </>
     );
   }
 }
