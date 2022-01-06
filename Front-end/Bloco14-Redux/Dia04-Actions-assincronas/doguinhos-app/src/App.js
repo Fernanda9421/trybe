bc80a20d6c8921ac178aa8e7dfd6113db4ee8a18
Fernanda9421 <fer.candrade21@gmail.com>
2022-01-06 15:00:05 -0300

    Configura mapStateToProps e mapDispatchToProps

--- Front-end/Bloco14-Redux/Dia04-Actions-assincronas/doguinhos-app/src/App.js
+++ Front-end/Bloco14-Redux/Dia04-Actions-assincronas/doguinhos-app/src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './redux/actions';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Doguinhos App</h1>
-      </div>
-    )
-  }
+function App({ isFetching, src, fetchDog }) {
+  return (
+    isFetching ? <p>Loading</p>
+      : (
+        <div style={{ width: 500 }}>
+          <button
+            style={{ display: 'block' }}
+            onClick={fetchDog}
+            type="button"
+          >
+            Novo Doguinho
+          </button>
+          <img style={{ width: '100%' }} src={src} alt="dog" />
+        </div>
+      )
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.reducer.imagePath,
+  isFetching: state.reducer.isFetching,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
