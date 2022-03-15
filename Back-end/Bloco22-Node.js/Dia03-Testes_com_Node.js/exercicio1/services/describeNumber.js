4c060aee90142303b69c7d5d195485116d1c9347
Fernanda9421 <fer.candrade21@gmail.com>
2022-03-15 19:14:36 -0300

    feat: exercicio 2 - tratamento para quando o parametro passado nao for do tipo 'number'

--- Back-end/Bloco22-Node.js/Dia03-Testes_com_Node.js/exercicio1/services/describeNumber.js
+++ Back-end/Bloco22-Node.js/Dia03-Testes_com_Node.js/exercicio1/services/describeNumber.js
@@ -1,11 +1,12 @@
 const describeNumber = (number) => {
   if (number > 0) {
-    return "positivo"
+    return "positivo";
   } else if (number < 0) {
-    return "negativo"
-  } else {
-    return "nulo"
+    return "negativo";
+  } else if (number === 0) {
+    return "nulo";
   }
+  return "o valor deve ser um número";
 }
 
 module.exports = describeNumber;
--- Back-end/Bloco22-Node.js/Dia03-Testes_com_Node.js/exercicio1/tests/numbers.test.js
+++ Back-end/Bloco22-Node.js/Dia03-Testes_com_Node.js/exercicio1/tests/numbers.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const describeNumber = require('../services/describeNumber');
+const VALOR_INVALIDO = "o valor deve ser um número";
 
 describe('describeNumber', () => {
   describe('Quando o número for maior que 0', () => {
@@ -40,6 +41,20 @@ describe('describeNumber', () => {
       it('é igual à "nulo"', () => {
         const answer = describeNumber(0);
         expect(answer).to.be.equals('nulo');
+      });
+    });
+  });
+
+  describe('Quando o valor passado não é um número', () => {
+    describe('A resposta', () => {
+      it('é uma string', () => {
+        const answer = describeNumber('número');
+        expect(answer).to.be.a('string');
+      });
+
+      it('é igual à "o valor deve ser um número"', () => {
+        const answer = describeNumber('número');
+        expect(answer).to.be.equals(VALOR_INVALIDO);
       });
     });
   });
