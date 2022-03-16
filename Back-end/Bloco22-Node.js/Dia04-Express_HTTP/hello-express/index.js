7098c85affa226af1a16bf455c70e45b75918c78
Fernanda9421 <fer.candrade21@gmail.com>
2022-03-16 10:43:23 -0300

    Exemplo de criaçao de API utilizando o Express

---
+++ Back-end/Bloco22-Node.js/Dia04-Express_HTTP/hello-express/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão ao molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/recipes', (_req, res) => {
+  res.json(recipes)
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
