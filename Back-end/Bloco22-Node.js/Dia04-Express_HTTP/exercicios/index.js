2b78314ce4fcf54d7338f302fe75e4299f6bc420
Fernanda9421 <fer.candrade21@gmail.com>
2022-03-17 08:54:59 -0300

    feat: exercicio 3 - cria rota POST

--- Back-end/Bloco22-Node.js/Dia04-Express_HTTP/exercicios/index.js
+++ Back-end/Bloco22-Node.js/Dia04-Express_HTTP/exercicios/index.js
@@ -4,13 +4,24 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+// exercício 1
 app.get('/ping', (req,res) => {
   return res.status(200).json({ message: 'pong' });
 });
 
+// exercício 2
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   return res.status(201).json({ message: `Hello, ${ name }!` });
+});
+
+// exercício 3
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (parseInt(age) <= 17) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.status(200).json({ message: `Hello, ${ name }!` });
 });
 
 app.listen(3001, () => {
