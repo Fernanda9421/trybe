1567f65db31693aa08a317986fb7b89e87a7e2b9
Fernanda9421 <fer.candrade21@gmail.com>
2022-03-21 09:09:33 -0300

    feat: exercise 2

--- Back-end/Bloco22-Node.js/exercicios/exercicios/index.js
+++ Back-end/Bloco22-Node.js/exercicios/exercicios/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 const pong = require('./middlewares/ping');
+const hello = require('./middlewares/hello');
 
 app.get('/ping', pong);
+app.post('/hello', hello);
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000');
