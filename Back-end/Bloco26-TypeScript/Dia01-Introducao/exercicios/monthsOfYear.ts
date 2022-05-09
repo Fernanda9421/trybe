e60c525488f45a6296ab21af689344200f815ed6
Fernanda9421 <fer.candrade21@gmail.com>
2022-05-09 13:36:12 -0300

    feat: exercício 2

---
+++ Back-end/Bloco26-TypeScript/Dia01-Introducao/exercicios/monthsOfYear.ts
@@ -0,0 +1,14 @@
+enum MonthsOfYear {
+  January = 'January',
+  February = 'February',
+  March = 'March',
+  April = 'April',
+  Mai = 'Mai',
+  June = 'June',
+  July = 'July',
+  August = 'August',
+  September = 'September',
+  October = 'October',
+  November = 'November',
+  December = 'December',
+}
