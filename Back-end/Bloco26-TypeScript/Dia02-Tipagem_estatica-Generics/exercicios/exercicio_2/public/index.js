3c619feeee48b0f67ae9bb4e5d0fa8f047bf5bb9
Fernanda9421 <fer.candrade21@gmail.com>
2022-05-12 13:45:19 -0300

    feat: exercicio 2

---
+++ Back-end/Bloco26-TypeScript/Dia02-Tipagem_estatica-Generics/exercicios/exercicio_2/public/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pizzaCalabresa = {
+    flavor: 'Calabresa',
+    slices: 8,
+};
+const pizzaMarguerita = {
+    flavor: 'Marguerita',
+    slices: 6,
+};
+const pizzaNutella = {
+    flavor: 'Nutella',
+    slices: 4,
+};
+console.log(pizzaCalabresa);
---
+++ Back-end/Bloco26-TypeScript/Dia02-Tipagem_estatica-Generics/exercicios/exercicio_2/src/index.ts
@@ -0,0 +1,18 @@
+import { Slices, Pizza } from "./pizza";
+
+const pizzaCalabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8,
+}
+
+const pizzaMarguerita: Pizza = {
+  flavor: 'Marguerita',
+  slices: 6,
+}
+
+const pizzaNutella: Pizza = {
+  flavor: 'Nutella',
+  slices: 4,
+}
+
+console.log(pizzaCalabresa);
