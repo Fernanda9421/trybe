36c3aa39f7cf55aa62f0e1965c02bd2763150b23
Fernanda9421 <fer.candrade21@gmail.com>
2022-05-18 19:13:28 -0300

    feat: exercicio 1 - cria classe Person, seus atributos, métodos e validações

---
+++ Back-end/Bloco27-POO_SOLID/Dia02-Heranca_e_interfaces/exercicio/src/index.ts
@@ -0,0 +1,7 @@
+import Person from "./Person";
+
+const person1 = new Person('Fernanda', new Date('1994/09/21'));
+const person2 = new Person('Paulo', new Date('1991/07/03'));
+
+console.log(person1);
+console.log(person2);
